048a678ba3377bd9b9cb80ac7eca25166655bf61
erin-day-2 <daye4@mail.uc.edu>
2022-02-04 15:29:42 -0500

    finished lab

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main
 
     public static void main(String[] args)
     {
-	// write your code here
+	double summerMant = 2500;
+    double fallMant = 3000;
+    double winterMant = 1000;
+    double springMant = 2500;
+    double totalCost = 0;
+
+    totalCost = summerMant + fallMant + winterMant + springMant;
+
+    System.out.println("The yearly maintenance cost of summer: " + summerMant + " fall: " + fallMant + " winter: " + winterMant + " and spring: " + springMant + " add up to create a total maintenance cost of: " + totalCost);
     }
 }
